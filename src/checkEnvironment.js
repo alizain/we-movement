012c934fcea4c5fa0b358260bdb2e261f1ac8537
Alizain Feerasta <alizain.feerasta@gmail.com>
2018-08-03 22:43:41 -0400

    First commit

--- src/checkEnvironment.js
+++
@@ -1,6 +0,0 @@
-export function isInDevelopment() {
-  if (process.env.NODE_ENV === "development") {
-    return true
-  }
-  return false
-}
--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
 import axios from "axios"
 import React from "react"
 import ReactDOM from "react-dom"
-import { Provider } from "react-redux"
-import { Router } from "react-router-dom"
 import * as log from "loglevel"
-import { isInDevelopment } from "./checkEnvironment"
-import configureStore from "./configureStore"
-import App from "./containers/App"
+import App from "./views/App"
 import "./index.css"
-import history from "./history"
 
-log.setDefaultLevel("warn")
-if (isInDevelopment()) {
-  log.enableAll()
-}
+log.enableAll()
 
-function initReactApp(passdownState = {}) {
-  const store = configureStore(passdownState)
-  ReactDOM.render(
-    <Router history={history}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </Router>,
-    document.getElementById("root"),
-  )
-}
-
-initReactApp()
+ReactDOM.render(<App />, document.getElementById("root"))
